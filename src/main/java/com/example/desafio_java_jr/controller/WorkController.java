546482f2b3d3ff9cd02d21ce2fddcbcc76c64e23
Daslan Mateus <daslan@weg.net>
2025-06-13 22:44:54 -0300

    Add rule for null date

--- src/main/java/com/example/desafio_java_jr/controller/WorkController.java
+++ src/main/java/com/example/desafio_java_jr/controller/WorkController.java
@@ -4,7 +4,6 @@ import com.example.desafio_java_jr.entity.Work;
 import com.example.desafio_java_jr.service.WorkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
